e2cd0edd424e34e36f3f8f5c928bf875d3d744a2
EVGENY.JS <53107821+Evgeny19989@users.noreply.github.com>
2020-08-17 16:47:17 +0700

    Update SectionInfo.jsx

--- src/components/SectionInfo.jsx
+++ src/components/SectionInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {BrowserRouter as Router , Switch , Route ,  Link} from 'react-router-dom';
+
 import SectionInfoHeader from './SectionInfoHeader';
 import Title from './Title';
 import SectionInfoFooter from './SectionInfoFooter';
