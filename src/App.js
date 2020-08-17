503a312c1d355a74451dc8a3007cc408e3201e87
EVGENY.JS <53107821+Evgeny19989@users.noreply.github.com>
2020-08-17 16:49:37 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router , Switch , Route ,  Link} from 'react-router-dom';
 
 
 import SectionSlider from './components/SectionSlider';
