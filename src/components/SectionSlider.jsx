b7365ec7a47467bc5591a0c95182b9aae1bd4063
EVGENY.JS <53107821+Evgeny19989@users.noreply.github.com>
2020-08-17 16:48:17 +0700

    Update SectionSlider.jsx

--- src/components/SectionSlider.jsx
+++ src/components/SectionSlider.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {BrowserRouter as Router , Switch , Route ,  Link} from 'react-router-dom';
+
 import Slider from './Slider'
 import SectionSliderFooter from './SectionSliderFooter'
 
